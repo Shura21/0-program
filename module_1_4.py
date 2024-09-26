14789b55a8b53066243a28885e8125c27ae8796d
Shura21 <konshina21092018@gmail.com>
2024-09-26 17:11:17 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+
+the_number_of_completed_dz =(12)
+number_of_hours_spent=(1.5)
+Course_name=('Python')
+time_for_one_task=(1.5/12)
+print(the_number_of_completed_dz,number_of_hours_spent,Course_name,time_for_one_task)
