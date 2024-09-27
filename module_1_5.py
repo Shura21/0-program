7e9cfd6d5dd6dbd376b3af980c0d6a2274569f29
Shura21 <konshina21092018@gmail.com>
2024-09-27 23:12:44 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
-name= "example"
-print(name[0])
-print(name[-1])
-print(name[3:])
-print(name[::-1])
-print(name[0:7:2])
+example = "Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1:10:2])
