fc5ba8dd1fd64b78f123f2e333f8dea17689a6fb
Shura21 <konshina21092018@gmail.com>
2024-09-30 18:17:23 +0300

    Add files via upload

---
+++ module_1_5_5.py
@@ -0,0 +1,12 @@
+immutable_var ='apple','day',100,200,True,'list'
+print(immutable_var)
+immutable_var['day']= 'new day'# так как day находится в неизменяем кортеже .
+imutable_list=['apple','day',100,200,True,'list']
+imutable_list[1]=2024
+print(imutable_list)
+imutable_list.append('big bos')
+print(imutable_list)
+imutable_list.extend('string')
+print(imutable_list)
+imutable_list.remove(200)
+print(imutable_list)
